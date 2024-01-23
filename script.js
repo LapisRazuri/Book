29df8a662e0ed08b4ee6634cec174cd159dafa8a
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2024-01-23 08:59:34 +0100

    reate function addBookToLibrary

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -9,5 +11,20 @@ function Book(title, author, pages, read) {
     };
 }
 
+
+function addBookToLibrary (userBookTitle, userBookAuthor, userBookPages, userBookRead) {
+    const book = new Book(userBookTitle, userBookAuthor, userBookPages, userBookRead)
+    myLibrary.push(book);
+    console.log(myLibrary);
+}
+
 const book1 = new Book("Hobbit", "J.R.R Tolkien", "295", "not read yet")
 book1.info();
+
+
+const userBookTitle = prompt("What is the name of the book?");
+const userBookAuthor = prompt("Who is the author?");
+const userBookPages = prompt("How many pages?");
+const userBookRead = prompt("Read or not read yet?")
+
+addBookToLibrary(userBookTitle, userBookAuthor, userBookPages, userBookRead)
