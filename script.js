d4fc37022405ce1a1bb6363efdff9507a382016f
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2024-01-24 05:00:54 +0100

    Commentout userinput instead manually added books to library to check display of the books

--- script.js
+++ script.js
@@ -12,19 +12,47 @@ function Book(title, author, pages, read) {
 }
 
 
-function addBookToLibrary (userBookTitle, userBookAuthor, userBookPages, userBookRead) {
-    const book = new Book(userBookTitle, userBookAuthor, userBookPages, userBookRead)
+function addBookToLibrary (book) {
     myLibrary.push(book);
     console.log(myLibrary);
 }
 
+
+
+function displayBooks() {
+    myLibrary.forEach(book => console.table(book))
+}
+
+
+
+
+
+
 const book1 = new Book("Hobbit", "J.R.R Tolkien", "295", "not read yet")
-book1.info();
+const book2 = new Book("GarasuNoKamen", "Miuchi Suzue", "670", "read")
+const book3 = new Book("Inferno", "Dan Brown", "364", "read")
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+displayBooks();
 
 
-const userBookTitle = prompt("What is the name of the book?");
-const userBookAuthor = prompt("Who is the author?");
-const userBookPages = prompt("How many pages?");
-const userBookRead = prompt("Read or not read yet?")
 
-addBookToLibrary(userBookTitle, userBookAuthor, userBookPages, userBookRead)
+
+// Uncomment this if you want the user to input the books
+// const userBookTitle = prompt("What is the name of the book?");
+// const userBookAuthor = prompt("Who is the author?");
+// const userBookPages = prompt("How many pages?");
+// const userBookRead = prompt("Read or not read yet?")
+
+// displayBooks(addBookToLibrary(userBookTitle, userBookAuthor, userBookPages, userBookRead))
+
+
+// And change the addBookToLibrary to below
+// function addBookToLibrary (userBookTitle, userBookAuthor, userBookPages, userBookRead) {
+//     const book = new Book(userBookTitle, userBookAuthor, userBookPages, userBookRead)
+//     myLibrary.push(book);
+//     console.log(myLibrary);
+// }
